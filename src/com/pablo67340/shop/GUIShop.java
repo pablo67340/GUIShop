994e2e6ff2cec58a12962af3fb9839109ff77415
Bryce <pablo67340@gmail.com>
2014-05-02 11:23:13 -0400

    Added Menu. New configs etc.

--- src/com/pablo67340/shop/GUIShop.java
+++ src/com/pablo67340/shop/GUIShop.java
@@ -1,5 +1,7 @@
 package com.pablo67340.shop;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -14,11 +16,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -28,10 +34,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class GUIShop extends JavaPlugin implements Listener {
 	public Economy econ;
 	List<String> sellitems = new ArrayList<String>();
+	private FileConfiguration customConfig = null;
+	private File customConfigFile = null;
+	private File defaultConfigFile = null;
 	HashMap<Integer,Inventory> shopinv = new HashMap<Integer,Inventory>();
+	String shopn = "";
 	String tag = this.getConfig().getString("tag");
-	String title2 = this.getConfig().getString("title");
-	String title = ChatColor.translateAlternateColorCodes('&', title2);
+	String title = "";
+	String menun = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("menuname"));
+
 	public void onEnable(){
 		sellitems.clear();
 		shopinv.clear();
@@ -47,8 +58,8 @@ public class GUIShop extends JavaPlugin implements Listener {
 	public boolean onCommand(final CommandSender sender, Command command, String label, final String[] args) {
 		if(sender instanceof Player){
 			Player p = (Player) sender;
-			if(command.getName().equalsIgnoreCase("shop") && p.hasPermission("gshop.use")){
-				loadShop(p);
+			if(command.getName().equalsIgnoreCase("shop") && p.hasPermission("guishop.use")){
+				loadMenu(p);
 			}else{
 				p.sendMessage("§cNo Permission!");
 			}
@@ -74,13 +85,85 @@ public class GUIShop extends JavaPlugin implements Listener {
 		return econ != null;
 	}
 
+	public void loadMenu(Player plyr){
+		title = "";
+		shopn = "";
+		if (plyr.hasPermission("guishop.Use"))
+			if (this.getConfig().getList("Disabl"
+					+ "edWorlds").contains(plyr.getWorld().getName())) {
+				plyr.sendMessage(ChatColor.DARK_RED + "You cannot use the shop from this world!");
+			} else {
+				List Ls = new ArrayList();
+				Inventory chest = plyr.getPlayer().getServer().createInventory(null, this.getConfig().getInt("Rows") * 9, menun);
+
+				int[] row1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+				int[] row2 = { 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+				int[] row3 = { 19, 20, 21, 23, 24, 25, 26, 27, 28 };
+
+				for (int slot : row1) {
+					if (this.getConfig().getString(slot + ".Enabled") == "true") {
+						Ls.clear();
+						String Name = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString(slot + ".Name"));
+						if (this.getConfig().getString(slot + ".Desc") != "null") {
+							Ls.add(this.getConfig().getString(slot + ".Desc"));
+						}
+						if (plyr.hasPermission("guishop.slot." + slot)) {
+							chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(this.getConfig().getString(slot + ".Item")), 1), Name, Ls));
+						} else {
+							Ls.add("No permission");
+							chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(36), 1), Name, Ls));
+						}
+					}
+				}
+
+				if (this.getConfig().getInt("Rows") >= 2) {
+					for (int slot : row2) {
+						if (this.getConfig().getString(slot + ".Enabled") == "true") {
+							Ls.clear();
+							String Name = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString(slot + ".Name"));
+							if (this.getConfig().getString(slot + ".Desc") != "null") {
+								Ls.add(this.getConfig().getString(slot + ".Desc"));
+							}
+							if (plyr.hasPermission("guishop.slot." + slot)) {
+								chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(this.getConfig().getString(slot + ".Item")), 1), Name, Ls));
+							} else {
+								Ls.add("No permission");
+								chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(36), 1), Name, Ls));
+							}
+						}
+					}
+
+				}
+
+				if (this.getConfig().getInt("Rows") >= 3) {
+					for (int slot : row3) {
+						if (this.getConfig().getString(slot + ".Enabled") == "true") {
+							Ls.clear();
+							String Name = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString(slot + ".Name"));
+							if (this.getConfig().getString(slot + ".Desc") != "null") {
+								Ls.add(this.getConfig().getString(slot + ".Desc"));
+							}
+							if (plyr.hasPermission("guishop.slot." + slot)) {
+								chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(this.getConfig().getString(slot + ".Item")), 1), Name, Ls));
+							} else {
+								Ls.add("No permission");
+								chest.setItem(slot - 1, setName(new ItemStack(Material.getMaterial(36), 1), Name, Ls));
+							}
+						}
+					}
+				}
+
+				plyr.openInventory(chest);
+			}
+	}
+
 	@SuppressWarnings("deprecation")
-	public void loadShop(Player plyr)
-	{
+	public void loadShop(Player plyr){
+
+		shopinv.clear();
+
 		int row = 9;
 		int size = 5;
-
-		
 
 		if(title.length() > 16) {
 			title.substring(0,16);
@@ -98,39 +181,55 @@ public class GUIShop extends JavaPlugin implements Listener {
 				String price = "0";
 				String sell = "0";
 
-				if(this.getConfig().get(i+"") != null)
+				if(this.getCustomConfig().get(shopn+"."+i+"") != null)
 				{
-					List<String> nodes = this.getConfig().getStringList(i+"");
+					List<String> nodes = this.getCustomConfig().getStringList(shopn+"."+i+"");
 					if(nodes != null) {
 						for(int nodeapi=0;nodeapi<nodes.size();nodeapi++) {
 							if(nodes.get(nodeapi).contains("slot:"))
 							{
 								slot = nodes.get(nodeapi).replace("slot:", "");
+							}else{
+
 							}
 							if(nodes.get(nodeapi).contains("name:"))
 							{
 								name = nodes.get(nodeapi).replace("name:", "").replace("'", "");
+							}else{
+
 							}
 							if(nodes.get(nodeapi).contains("price:"))
 							{
 								price = nodes.get(nodeapi).replace("price:", "").replace("'", "");
+							}else{
+
 							}
 							if(nodes.get(nodeapi).contains("data:"))
 							{
 								data = nodes.get(nodeapi).replace("data:", "");
+							}else{
+
 							}
 							if(nodes.get(nodeapi).contains("qty:"))
 							{
 								qty = nodes.get(nodeapi).replace("qty:", "");
+							}else{
+
 							}
 							if(nodes.get(nodeapi).contains("sell:"))
 							{
 								sell = nodes.get(nodeapi).replace("sell:", "").replace("'", "");
-								if(!sellitems.contains(i+"$"+sell+")"+qty)) {
-									sellitems.add(i+"$"+sell+")"+qty);
+								if(!sellitems.contains(shopn+"."+i+"$"+sell+")"+qty)) {
+									sellitems.add(shopn+"."+i+"$"+sell+")"+qty);
+								}else{
+
 								}
+							}else{
+
 							}
 						}
+					}else{
+
 					}
 					if(Integer.parseInt(data) != 0)
 					{
@@ -162,11 +261,15 @@ public class GUIShop extends JavaPlugin implements Listener {
 					}
 					shopinv.put(1, shop);
 					plyr.openInventory(shop);
+				}else{
+
 				}
 				plyr.updateInventory();
 			}
 		}else{
+
 			plyr.openInventory(shopinv.get(1));
+
 		}
 	}
 
@@ -185,18 +288,59 @@ public class GUIShop extends JavaPlugin implements Listener {
 		item.setAmount(amount);
 		return item;
 	}
-
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onClick(InventoryClickEvent e)
 	{
-		
+
 		if(e.getWhoClicked() instanceof Player)
 		{
 
 			Player p = (Player) e.getWhoClicked();
 
-			if(e.getInventory().getTitle().contains(title))
-			{
+			if(e.getInventory().getTitle().contains(menun)){
+				if (e.getSlotType() == InventoryType.SlotType.CONTAINER) {
+					if (e.isLeftClick()) {
+						if (!e.isShiftClick())
+						{
+							int[] row1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28 };
+
+							for (int slot : row1)
+								if (e.getRawSlot() == slot - 1) {
+									if (this.getConfig().getString(slot + ".Enabled") == "true") {
+										if (p.hasPermission("guishop.slot." + slot)) {
+											e.setCancelled(true);
+											final String shop = this.getConfig().getString(slot + ".Shop");
+											title = "";
+											title = this.getConfig().getString(slot + ".Shop");
+											shopn = shop + ".";
+											loadShop(p);
+											e.setCancelled(true);
+										}else{
+											System.out.println("No permission for slots");
+										}
+										break;
+									}else{
+										System.out.println("Slot not enabled!");
+									}
+
+									e.setCancelled(true);
+									break;
+								}
+						}
+						else
+						{
+							e.setCancelled(true);
+						}
+					}
+					else {
+						e.setCancelled(true);
+					}
+				}
+				else
+					e.setCancelled(true);
+			}else{
+			}
+			if(e.getInventory().getTitle().contains(title)){
 				e.setCancelled(true);
 				p.closeInventory();
 
@@ -230,12 +374,15 @@ public class GUIShop extends JavaPlugin implements Listener {
 											p.sendMessage(ChatColor.translateAlternateColorCodes('&', tag) + " " + "§fYou need §c$" + dif + "§f more money.");
 										}
 									}
+								}else{
+									System.out.println("Items have no lore");
 								}
 							}
 							loadShop(p);
 						}
 					}
 				}
+			}else{
 			}
 		}
 	}
@@ -273,7 +420,52 @@ public class GUIShop extends JavaPlugin implements Listener {
 				}
 			}
 		}else{
-			
+
+		}
+	}
+	private ItemStack setName(ItemStack is, String name, List<String> lore){
+		ItemMeta IM = is.getItemMeta();
+		if (name != null) {
+			IM.setDisplayName(name);
+		}
+		if (lore != null) {
+			IM.setLore(lore);
+		}
+		is.setItemMeta(IM);
+		return is;
+	}
+
+	public void reloadCustomConfig() {
+		if (customConfigFile == null) {
+			customConfigFile = new File(getDataFolder(), "shops.yml");
+		}
+		customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+
+		// Look for defaults in the jar
+		InputStream defConfigStream = this.getResource("shops.yml");
+		if (defConfigStream != null) {
+			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+			customConfig.setDefaults(defConfig);
+		}
+	}
+
+	public FileConfiguration getCustomConfig() {
+		if (customConfig == null) {
+			reloadCustomConfig();
+		}
+		return customConfig;
+	}
+
+	public void saveDefaultConfig() {
+		if (customConfigFile == null) {
+			customConfigFile = new File(getDataFolder(), "shops.yml");
+			defaultConfigFile = new File(getDataFolder(), "config.yml");
+		}
+		if (!customConfigFile.exists()) {            
+			this.saveResource("shops.yml", false);
+		}
+		if (!defaultConfigFile.exists()) {            
+			this.saveResource("config.yml", false);
 		}
 	}
 }

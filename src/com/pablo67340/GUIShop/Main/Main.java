138eeb0a48529a4ae4c255a3046c9127bdf3fcdb
Bryce <pablo67340@gmail.com>
2016-07-22 21:03:37 -0400

    Inventory Logic Fixes
    FIXES:
    - Fixed inventories locking after closing shop without actions.

--- src/com/pablo67340/GUIShop/Main/Main.java
+++ src/com/pablo67340/GUIShop/Main/Main.java
@@ -1,6 +1,6 @@
 package com.pablo67340.GUIShop.Main;
 
-import com.pablo67340.GUIShop.Handlers.Cache;
+
 import com.pablo67340.GUIShop.Handlers.Item;
 import com.pablo67340.GUIShop.Handlers.Menu;
 import com.pablo67340.GUIShop.Handlers.Sell;
@@ -26,133 +26,132 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class Main
-extends JavaPlugin {
-    private FileConfiguration customConfig = null;
-    private File customConfigFile = null;
-    private File defaultConfigFile = null;
-    public Economy econ;
-    public Item item;
-    public Utils utils;
-    public Cache cache;
-    public Shop shop;
-    public Menu menu;
-    public Sell sell;
-    public List<String> sellitems;
+public class Main extends JavaPlugin {
+	private FileConfiguration customConfig = null;
+	private File customConfigFile = null;
+	private File defaultConfigFile = null;
+	public Economy econ;
+	public Item item;
+	public Utils utils;
+	public Shop shop;
+	public Menu menu;
+	public Sell sell;
+	public List<String> sellitems;
+	public PlayerListener listene = new PlayerListener(this);
 
-    public Main() {
-        item = new Item(this);
-        utils = new Utils(this);
-        cache = new Cache(this);
-        shop = new Shop(this);
-        menu = new Menu(this);
-        sell = new Sell(this);
-        sellitems = new ArrayList<String>();
-    }
+	public Main() {
+		item = new Item(this);
+		utils = new Utils(this);
+		shop = new Shop(this);
+		menu = new Menu(this);
+		sell = new Sell(this);
+		sellitems = new ArrayList<String>();
+	}
 
-    public void onEnable() {
-        if (!setupEconomy()) {
-            getServer().getPluginManager().disablePlugin((Plugin)this);
-            return;
-        }
-        saveDefaultConfig();
-        loadDefaults();
-        getServer().getPluginManager().registerEvents((Listener)new PlayerListener(this), (Plugin)this);
-    }
+	public void onEnable() {
+		if (!setupEconomy()) {
+			System.out.println("Plugin couldnt detect Vault or a Economy plugin (Such as Essentials ECO). Disabling.");
+			getServer().getPluginManager().disablePlugin((Plugin)this);
+			return;
+		}
+		saveDefaultConfig();
+		loadDefaults();
+		getServer().getPluginManager().registerEvents((Listener)listene, (Plugin)this);
+	}
 
-    public void onDisable() {
-        cache.flushData();
-    }
+	public void onDisable() {
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider rsp = getServer().getServicesManager().getRegistration((Class)Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = (Economy)rsp.getProvider();
-        if (econ != null) {
-            return true;
-        }
-        return false;
-    }
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider rsp = getServer().getServicesManager().getRegistration((Class)Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = (Economy)rsp.getProvider();
+		if (econ != null) {
+			return true;
+		}
+		return false;
+	}
 
-    public void loadDefaults() {
-        utils.setCommand(getConfig().getString("Command"));
-        utils.setMenuName(ChatColor.translateAlternateColorCodes('&', getConfig().getString("menuname")));
-        utils.setPrefix(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tag")));
-        utils.setSignOnly(getConfig().getBoolean("sign-only"));
-        utils.setSignTitle(ChatColor.translateAlternateColorCodes('&', getConfig().getString("sign-title")));
-        utils.setVerbose(getConfig().getBoolean("Verbose"));
-        utils.setSellCommand(getConfig().getString("sell-command"));
-        utils.setSellTitle(getConfig().getString("sell-title"));
-    }
+	public void loadDefaults() {
+		utils.setCommand(getConfig().getString("Command"));
+		utils.setMenuName(ChatColor.translateAlternateColorCodes('&', getConfig().getString("menuname")));
+		utils.setPrefix(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tag")));
+		utils.setSignOnly(getConfig().getBoolean("sign-only"));
+		utils.setSignTitle(ChatColor.translateAlternateColorCodes('&', getConfig().getString("sign-title")));
+		utils.setVerbose(getConfig().getBoolean("Verbose"));
+		utils.setSellCommand(getConfig().getString("sell-command"));
+		utils.setSellTitle(getConfig().getString("sell-title"));
+	}
 
-    public void closeInventory(final Player p) {
-        getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
+	public void closeInventory(final Player p) {
+		getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
 
-            @Override
-            public void run() {
-                p.closeInventory();
-            }
-        }, 1);
-    }
+			@Override
+			public void run() {
+				p.closeInventory();
+			}
+		}, 1);
+	}
 
-    public void delayMenu(final Player p) {
-        getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
+	public void delayMenu(final Player p) {
+		getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
 
-            @Override
-            public void run() {
-                menu.loadMenu(p);
-            }
-        }, 1);
-    }
-    
-    public void delayShop(final Player p) {
-        getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
+			@Override
+			public void run() {
+				menu.openMenu(p);
+			}
+		}, 1);
+	}
 
-            @Override
-            public void run() {
-                shop.loadShop(p);
-            }
-        }, 1);
-    }
+	public void delayShop(final Player p) {
+		getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, new Runnable(){
 
-    @SuppressWarnings("deprecation")
+			@Override
+			public void run() {
+				shop.openShop(p);
+			}
+		}, 1);
+	}
+
+	@SuppressWarnings("deprecation")
 	public void reloadCustomConfig() {
-        if (customConfigFile == null) {
-            customConfigFile = new File(getDataFolder(), "shops.yml");
-        }
-        customConfig = YamlConfiguration.loadConfiguration((File)customConfigFile);
-        InputStream defConfigStream = getResource("shops.yml");
-        if (defConfigStream != null) {
-            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration((InputStream)defConfigStream);
-            customConfig.setDefaults((Configuration)defConfig);
-        }
-    }
+		if (customConfigFile == null) {
+			customConfigFile = new File(getDataFolder(), "shops.yml");
+		}
+		customConfig = YamlConfiguration.loadConfiguration((File)customConfigFile);
+		InputStream defConfigStream = getResource("shops.yml");
+		if (defConfigStream != null) {
+			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration((InputStream)defConfigStream);
+			customConfig.setDefaults((Configuration)defConfig);
+		}
+	}
 
-    public FileConfiguration getCustomConfig() {
-        if (customConfig == null) {
-            reloadCustomConfig();
-        }
-        return customConfig;
-    }
+	public FileConfiguration getCustomConfig() {
+		if (customConfig == null) {
+			reloadCustomConfig();
+		}
+		return customConfig;
+	}
 
-    public void saveDefaultConfig() {
-        if (customConfigFile == null) {
-            customConfigFile = new File(getDataFolder(), "shops.yml");
-            defaultConfigFile = new File(getDataFolder(), "config.yml");
-        }
-        if (!customConfigFile.exists()) {
-            saveResource("shops.yml", false);
-        }
-        if (!defaultConfigFile.exists()) {
-            saveResource("config.yml", false);
-        }
-    }
+	public void saveDefaultConfig() {
+		if (customConfigFile == null) {
+			customConfigFile = new File(getDataFolder(), "shops.yml");
+			defaultConfigFile = new File(getDataFolder(), "config.yml");
+		}
+		if (!customConfigFile.exists()) {
+			saveResource("shops.yml", false);
+		}
+		if (!defaultConfigFile.exists()) {
+			saveResource("config.yml", false);
+		}
+	}
 
 }
 
